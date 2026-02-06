aca93d2902af751eea023c199c4c3cb3a80f5eb2
minecraft0036 <aosi90736@gmail.com>
2026-02-06 20:56:26 +0900

    Add AntiFileDelete .NET project and build outputs
    Add a new AntiFileDelete .NET 10 Windows project: AntiFileDelete.csproj, a simple Program.cs (opens a file stream and holds it to prevent deletion), and publish profile files. The project enables AOT and ReadyToRun and configures single-file publishing for win-x64. This changeset also includes generated build and publish artifacts (bin/, obj/, runtimeconfig/deps files, DLL/EXE/PDBs) and user-specific .csproj.user and publish profile user files.

---
+++ CSarpSorce/AntiFileDelete/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AntiFileDelete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var stream = new FileStream(args[0], FileMode.Open, FileAccess.Read, FileShare.Read);
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
